b91636e44374b0963026b9da1692e136c834b92d
R. de Wilde <email@robertdewilde.nl>
2016-05-19 20:08:37 +0200

    Unused variables

--- src/key.cpp
+++ src/key.cpp
@@ -549,7 +549,7 @@ bool CKey::SignCompact(const uint256 &hash, std::vector<unsigned char>& vchSig) 
     CKey nonce;
     do {
         nonce.MakeNewKey(true);
-        if (int ret = secp256k1_ecdsa_sign_compact(instance_of_csecp256k1.ctx, hash.begin(), &vchSig[1], begin(), secp256k1_nonce_function_rfc6979, NULL, &rec))
+        if (secp256k1_ecdsa_sign_compact(instance_of_csecp256k1.ctx, hash.begin(), &vchSig[1], begin(), secp256k1_nonce_function_rfc6979, NULL, &rec))
             break;
     } while(true);
 #else
@@ -629,7 +629,7 @@ bool CPubKey::VerifyCompact(const uint256 &hash, const std::vector<unsigned char
     if (vchSig.size() != 65)
         return false;
     int recid = (vchSig[0] - 27) & 3;
-    bool fComp = IsCompressed();
+//    bool fComp = IsCompressed();
     CPubKey pubkeyRec;
 /*#ifdef USE_SECP256K1
     int pubkeylen = 65;
